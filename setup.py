57642cd3e4f8f37f770e40732fafefec2d09f842
OrthodoxJesus <74015647+OrthodoxJesus@users.noreply.github.com>
2023-03-29 13:58:11 +0300

    Add files via upload
    Scripts done for mps classifier on local PC

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='classificator',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='User',
+    author_email='',
+    description=''
+)
